08483824782cd436cb7d0f50bbc5fce28ab535a8
姚双飞 <18767176093@163.com>
2017-06-09 08:54:05 +0800

    Add files via upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,78 @@
 <template>
 	<el-row class="container">
-		<el-col :span="24" class="header">
-			<el-col :span="4" class="logo" :class="collapsed?'logo-collapse-width':'logo-width'"></el-col>
+	<!-- 	<el-col :span="24" class="header">
+			<el-col :span="10" class="logo" :class="collapsed?'logo-collapse-width':'logo-width'">
+				吸引力
+			</el-col>
+			<el-col :span="10">
+				<div class="tools" @click.prevent="collapse">
+					<i class="fa fa-align-justify"></i>
+				</div>
+			</el-col>
+			<el-col :span="4" class="userinfo">
+				<el-dropdown trigger="hover">
+					<span class="el-dropdown-link userinfo-inner">
+					<img :src="this.sysUserAvatar" /> 
+					你好:
+					{{sysUserName}}</span>
+					<el-dropdown-menu slot="dropdown">
+						<el-dropdown-item>我的消息</el-dropdown-item>
+						<el-dropdown-item>设置</el-dropdown-item>
+						<el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>
+					</el-dropdown-menu>
+				</el-dropdown>
+			</el-col>
+		</el-col> -->
+		<el-col :span="24" class="main">
+			<aside :class="collapsed?'menu-collapsed':'menu-expanded'">
+				<div style="height: 158px"></div>
+				<!--导航菜单-->
+				<!-- <el-menu :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen" @close="handleclose" @select="handleselect"
+					 unique-opened router v-show="!collapsed">
+					<template v-for="(item,index) in meunList" >
+						<el-submenu>
+							<template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
+							<el-menu-item v-for="child in item.children" v-if="!child.hidden">{{child.name}}</el-menu-item>
+						</el-submenu>
+						<el-menu-item v-if="item.leaf&&item.children.length>0" ><i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
+					</template>
+				</el-menu> -->
+				<div style="width: 230px;height: 80%;overflow-y: auto;">
+					
+				
+				<el-menu :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen" @close="handleclose" @select="handleselect"
+					 unique-opened router v-show="!collapsed">
+					<template  v-for="(item,index) in $router.options.routes"  v-if="!item.hidden">
+						<el-submenu :index="index+''" v-if="!item.leaf">
+							<template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
+							<el-menu-item v-for="child in item.children" :index="child.path" v-if="!child.hidden">{{child.name}}</el-menu-item>
+						</el-submenu>
+						<el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path"><i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
+					</template>
+				</el-menu>
+				<!--导航菜单-折叠后-->
+				<ul class="el-menu el-menu-vertical-demo collapsed" v-show="collapsed" ref="menuCollapsed">
+					<li v-for="(item,index) in $router.options.routes" v-if="!item.hidden" class="el-submenu item">
+						<template v-if="!item.leaf">
+							<div class="el-submenu__title" style="padding-left: 20px;" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)"><i :class="item.iconCls"></i></div>
+							<ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)" @mouseout="showMenu(index,false)">
+								<li v-for="child in item.children" v-if="!child.hidden" class="el-menu-item" style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''" @click="$router.push(child.path)">{{child.name}}</li>
+							</ul>
+						</template>
+						<template v-else>
+							<li class="el-submenu">
+								<div class="el-submenu__title el-menu-item" style="padding-left: 20px;height: 56px;line-height: 56px;padding: 0 20px;" :class="$route.path==item.children[0].path?'is-active':''" @click="$router.push(item.children[0].path)"><i :class="item.iconCls"></i></div>
+							</li>
+						</template>
+					</li>
+				</ul></div>
+			</aside>
+			<section class="content-container">
+				<div style="width: 1654px;position: fixed;z-index: 9999;top:-10px">
+						<el-col :span="24" class="header">
+			<el-col :span="10" class="logo" :class="collapsed?'logo-collapse-width':'logo-width'">
+				吸引力
+			</el-col>
 			<el-col :span="10">
 				<div class="tools" @click.prevent="collapse">
 					<i class="fa fa-align-justify"></i>
@@ -21,48 +92,7 @@
 				</el-dropdown>
 			</el-col>
 		</el-col>
-		<el-col :span="24" class="main">
-			<aside :class="collapsed?'menu-collapsed':'menu-expanded'">
-				<!--导航菜单-->
-				<el-menu :default-active="$route.path" class="el-menu-vertical-demo" @open="handleopen" @close="handleclose" @select="handleselect"
-					 unique-opened router v-show="!collapsed">
-					<!--<div class="main_left_logo"></div>-->
-					<template  v-for="(item,index) in $router.options.routes"  v-if="!item.hidden">
-						<el-submenu :index="index+''" v-if="!item.leaf">
-							<template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
-							<el-menu-item v-for="child in item.children" :index="child.path" v-if="!child.hidden">{{child.name}}</el-menu-item>
-						</el-submenu>
-						<el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path"><i :class="item.iconCls"></i>{{item.children[0].name}}</el-menu-item>
-					</template>
-				</el-menu>
-				<!--导航菜单-折叠后-->
-				<ul class="el-menu el-menu-vertical-demo collapsed" v-show="collapsed" ref="menuCollapsed">
-					<li v-for="(item,index) in $router.options.routes" v-if="!item.hidden" class="el-submenu item">
-						<template v-if="!item.leaf">
-							<div class="el-submenu__title" style="padding-left: 20px;" @mouseover="showMenu(index,true)"
-								 @mouseout="showMenu(index,false)"><i :class="item.iconCls"></i></div>
-							<ul class="el-menu submenu" :class="'submenu-hook-'+index" @mouseover="showMenu(index,true)"
-								@mouseout="showMenu(index,false)">
-								<li v-for="child in item.children" v-if="!child.hidden" class="el-menu-item"
-									style="padding-left: 40px;" :class="$route.path==child.path?'is-active':''"
-									@click="$router.push(child.path)">{{child.name}}
-								</li>
-							</ul>
-						</template>
-						<template v-else>
-							<!--<li class="el-submenu">-->
-								<!--<div class="el-submenu__title el-menu-item"-->
-									 <!--style="padding-left: 20px;height: 56px;line-height: 56px;padding: 0 20px;"-->
-									 <!--:class="$route.path==item.children[0].path?'is-active':''"-->
-									 <!--@click="$router.push(item.children[0].path)">-->
-									<!--<i :class="item.iconCls"></i>-->
-								<!--</div>-->
-							<!--</li>-->
-						</template>
-					</li>
-				</ul>
-			</aside>
-			<section class="content-container">
+				</div>
 				<div class="grid-content bg-purple-light">
 					<el-col :span="24" class="breadcrumb-container">
 						<strong class="title">{{$route.name}}</strong>
@@ -80,6 +110,11 @@
 				</div>
 			</section>
 		</el-col>
+		<!-- <el-col :span="24" class="footer">
+			<el-row>
+				<el-col :span="24">11111</el-col>
+			</el-row>
+		</el-col> -->
 	</el-row>
 </template>
 
@@ -87,7 +122,7 @@
 	export default {
 		data() {
 			return {
-				sysName:'后台超级管理系统',
+				sysName:'蜜蜂聚财',
 				collapsed:false,
 				sysUserName: '',
 				sysUserAvatar: '',
@@ -214,9 +249,8 @@
 		.header {
 			height: 60px;
 			line-height: 60px;
-			background: $color-primarys;
+			background: $color-primary;
 			color:#fff;
-			margin-top: inherit;
 			.userinfo {
 				text-align: right;
 				padding-right: 35px;
@@ -239,11 +273,9 @@
 				font-size: 22px;
 				padding-left:20px;
 				padding-right:20px;
-				background: $color-header;
-				margin-top: auto;
-				/*border-color: rgba(238,241,146,0.3);*/
-				/*border-right-width: 1px;*/
-				/*border-right-style: solid;*/
+				border-color: rgba(238,241,146,0.3);
+				border-right-width: 1px;
+				border-right-style: solid;
 				img {
 					width: 40px;
 					float: left;
@@ -272,22 +304,17 @@
 		}
 		.main {
 			display: flex;
-			// background: #324057;
+			/*// background: #324057;*/
 			position: absolute;
-			top: 60px;
+			top: -10px;
 			bottom: 0px;
 			overflow: hidden;
-			margin-top: inherit;
 			aside {
 				flex:0 0 230px;
 				width: 230px;
-				// position: absolute;
-				// top: 0px;
-				// bottom: 0px;
-				.main_left_logo {
-					height:230px;
-					background: $color-header;
-				}
+				/*// position: absolute;*/
+				/*// top: 0px;*/
+				/*// bottom: 0px;*/
 				.el-menu{
 					height: 100%;
 				}
@@ -315,16 +342,16 @@
 			.menu-expanded{
 				flex:0 0 230px;
 				width: 230px;
-				overflow-y: auto;
+				/*overflow-y: auto;*/
 			}
 			.content-container {
-				// background: #f1f2f7;
+				/*// background: #f1f2f7;*/
 				flex:1;
-				// position: absolute;
+			/*	// position: absolute;
 				// right: 0px;
 				// top: 0px;
 				// bottom: 0px;
-				// left: 230px;
+				// left: 230px;*/
 				overflow-y: scroll;
 				padding: 20px;
 				.breadcrumb-container {
