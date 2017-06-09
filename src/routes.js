75936edb9a7ba34fa35f6be8043c1ec4c6f868e4
Yaoshuangfei <18767176093@163.com>
2017-06-09 11:07:18 +0800

    up

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import Login from './views/Login.vue'
 import NotFound from './views/404.vue'
 import Home from './views/Home.vue'
-import Main from './views/Main.vue'
+import Main from './views/Home/indexMain.vue'
 import { state } from './vuex/state'
 /*标的管理Subject*/
 import subjectCheckPending from './views/Subject/subjectCheckPending.vue'
@@ -83,45 +83,51 @@ let routes = [
         name: '',
         hidden: true
     },
-        //{ path: '/main', component: Main },
+     {
+        path: '/mian',
+        component: Main,
+        name: '',
+        hidden: true
+    },
+    // { path: '/main', component: Main },
     {
         path: '/Subject',
         component: Home,
-        name: ' 标的管理',
+        name: ' 系统设置',
         iconCls: 'el-icon-message',//图标样式class
         children: [
             { path: '/main', component: Main, name: '主页', hidden: true },
-            { path: '/subjectCheckPending', component: subjectCheckPending, name: '待审核标的列表' },
-            { path: '/subjectChecked', component: subjectChecked, name: '已审核标的列表' },
-            { path: '/pending/:borrowId/', component: pending, name: '审核标的', hidden: true},
-            { path: '/pending/:borrowId/:subjectId/', component: pending, name: '查看或发布标的', hidden: true},
-            // { path: '/pending/:borrowId/:subjectId/', component: pending, name: '发布标的', hidden: true},
-            { path: '/pending/:borrowId/:subjectId/:checked/', component: pending, name: '查看已审核标的', hidden: true},
-            { path: '/subjectList', component: subjectList, name: '标的管理列表' },
-            { path: '/subjectRecord', component: subjectRecord, name: '标的购买记录' },
-            { path: '/subjectPlanList', component: subjectPlanList, name: '蜜蜂计划管理列表' },
-            { path: '/subjectPlanRecord', component: subjectPlanRecord, name: '蜜蜂计划购买记录' },
-            { path: '/transferRecord', component: transferRecord, name: '转让标的列表' }
+            { path: '/subjectCheckPending', component: subjectCheckPending, name: '权限管理' },
+            { path: '/subjectChecked', component: subjectChecked, name: '更改密码' },
+            // { path: '/pending/:borrowId/', component: pending, name: '审核标的', hidden: true},
+            // { path: '/pending/:borrowId/:subjectId/', component: pending, name: '查看或发布标的', hidden: true},
+            // // { path: '/pending/:borrowId/:subjectId/', component: pending, name: '发布标的', hidden: true},
+            // { path: '/pending/:borrowId/:subjectId/:checked/', component: pending, name: '查看已审核标的', hidden: true},
+            // { path: '/subjectList', component: subjectList, name: '标的管理列表' },
+            // { path: '/subjectRecord', component: subjectRecord, name: '标的购买记录' },
+            // { path: '/subjectPlanList', component: subjectPlanList, name: '蜜蜂计划管理列表' },
+            // { path: '/subjectPlanRecord', component: subjectPlanRecord, name: '蜜蜂计划购买记录' },
+            { path: '/transferRecord', component: transferRecord, name: '系统推送' }
         ]
     },
     {
         path: '/',
         component: Home,
-        name: '财务管理',
+        name: '配置管理',
         iconCls: 'fa fa-bar-chart',
         children: [
-            { path: '/withdrawList', component: withdrawList, name: '提现待审核' },
-            { path: '/withdrawFinishedList', component: withdrawFinishedList, name: '提现管理' },
-            { path: '/withdrawPlayinList', component: withdrawPlayinList, name: '提现打款' },
-            { path: '/checkList', component: checkList, name: '付款管理' },
-            { path: '/usedRecord', component: usedRecord, name: '资金使用记录' },
-            { path: '/investRecord', component: investRecord, name: '投标记录列表' }
+            { path: '/withdrawList', component: withdrawList, name: 'banner管理了' },
+            { path: '/withdrawFinishedList', component: withdrawFinishedList, name: '系统配置' },
+            { path: '/withdrawPlayinList', component: withdrawPlayinList, name: '银行卡管理' },
+            { path: '/checkList', component: checkList, name: '规则管理' }
+            // { path: '/usedRecord', component: usedRecord, name: '资金使用记录' },
+            // { path: '/investRecord', component: investRecord, name: '投标记录列表' }
         ]
     },
     {
         path: '/',
         component: Home,
-        name: '投资人管理',
+        name: '商品管理',
         iconCls: 'fa fa-id-card-o',
         children: [
             { path: '/investorList', component: investorList, name: '投资人管理列表' },
@@ -132,52 +138,58 @@ let routes = [
     {
         path: '/',
         component: Home,
-        name: '资讯管理',
+        name: '订单管理',
         iconCls: 'fa el-icon-picture',
         children: [
-            { path: '/articleList', component: articleList, name: '文章列表' },
-            { path: '/addArticle/:id', component: addArticle, name: '添加文章' },
-            { path: '/newsList/:id', component: newsList, name: '新闻资讯' },
-            { path: '/advertisementManagement', component: advertisementManagement, name: 'App开屏广告管理' },
-            { path: '/carPCouselManagementApp', component: carPCouselManagementApp, name: 'App轮播图管理' },
-            { path: '/menuManagement', component: menuManagement, name: 'App菜单管理' },
-            { path: '/carouselManagementPC', component: carouselManagementPC, name: 'PC轮播图管理' },
-            { path: '/bankManagement', component: bankManagement, name: '支付银行说明管理' }
+            { path: '/articleList', component: articleList, name: '订单信息管理' },
+            // { path: '/addArticle/:id', component: addArticle, name: '添加文章' },
+            // { path: '/newsList/:id', component: newsList, name: '新闻资讯' },
+            // { path: '/advertisementManagement', component: advertisementManagement, name: 'App开屏广告管理' },
+            // { path: '/carPCouselManagementApp', component: carPCouselManagementApp, name: 'App轮播图管理' },
+            // { path: '/menuManagement', component: menuManagement, name: 'App菜单管理' },
+            { path: '/carouselManagementPC', component: carouselManagementPC, name: '订单评价管理' },
+            { path: '/bankManagement', component: bankManagement, name: '退货管理' }
         ]
     },
     {
         path: '/',
         component: Home,
-        name: '活动管理',
+        name: '店铺管理',
         iconCls: 'fa el-icon-menu',
         children: [
-            { path: '/onlineCoupon', component: onlineCoupon, name: '线上理财礼券', hidden: true },
-            { path: '/packageList', component: packageList, name: '礼包管理' },
-            { path: '/sendPackage', component: sendPackage, name: '发送红包管理' },
-            { path: '/underlineCoupon', component: underlineCoupon, name: '线下理财券', hidden: true },
-            { path: '/underlineCouponAdd', component: underlineCouponAdd, name: '添加线下理财券', hidden: true },
-            { path: '/underlineCouponAdd/:id', component: underlineCouponAdd, name: '修改线下理财券', hidden: true },
-            { path: '/AllocationFinancialProductCoupon/:id', component: AllocationFinancialProductCoupon, name: '分派线下理财券', hidden: true },
-            { path: '/messageSend', component: messageSend, name: '短信发送管理' },
-            { path: '/messageTemplate', component: messageTemplate, name: '短信模板管理' },
-            { path: '/appMessagePush', component: appMessagePush, name: 'App消息推送', hidden: true },
-            { path: '/sourceManage', component: sourceManage, name: '渠道管理' },
-            { path: '/dataStatistics', component: dataStatistics, name: '渠道数据统计' },
-            { path: '/mallList', component: mallList, name: '积分商城商品管理' },
-            { path: '/exchangeRecord', component: exchangeRecord, name: '积分商城兑换记录' },
+            // { path: '/onlineCoupon', component: onlineCoupon, name: '线上理财礼券', hidden: true },
+            { path: '/packageList', component: packageList, name: '店铺信息管理' },
+            { path: '/sendPackage', component: sendPackage, name: '视频管理' },
+            // { path: '/underlineCoupon', component: underlineCoupon, name: '线下理财券', hidden: true },
+            // { path: '/underlineCouponAdd', component: underlineCouponAdd, name: '添加线下理财券', hidden: true },
+            // { path: '/underlineCouponAdd/:id', component: underlineCouponAdd, name: '修改线下理财券', hidden: true },
+            // { path: '/AllocationFinancialProductCoupon/:id', component: AllocationFinancialProductCoupon, name: '分派线下理财券', hidden: true },
+            // { path: '/messageSend', component: messageSend, name: '短信发送管理' },
+            // { path: '/messageTemplate', component: messageTemplate, name: '短信模板管理' },
+            // { path: '/appMessagePush', component: appMessagePush, name: 'App消息推送', hidden: true },
+            // { path: '/sourceManage', component: sourceManage, name: '渠道管理' },
+            // { path: '/dataStatistics', component: dataStatistics, name: '渠道数据统计' },
+            // { path: '/mallList', component: mallList, name: '积分商城商品管理' },
+            { path: '/exchangeRecord', component: exchangeRecord, name: '违规处理' },
         ]
     },
     {
         path: '/',
         component: Home,
-        name: '理财管理',
+        name: '财务管理',
         iconCls: 'fa el-icon-date',
         children: [
-            { path: '/productList', component: productList, name: '理财产品管理列表' },
+            { path: '/productList', component: productList, name: '网站流水报表' },
             { path: '/productAdd', component: productAdd, name: '理财产品添加', hidden: true },
             { path: '/productAdd/:id', component: productAdd, name: '理财产品修改', hidden: true },
-            { path: '/producRecordtList', component: producRecordtList, name: '理财产品购买记录' },
-            { path: '/unmatchProductList', component: unmatchProductList, name: '理财产品未匹配标的列表' }
+            { path: '/producRecordtList', component: producRecordtList, name: '手机充值记录' },
+            { path: '/unmatchProductList', component: unmatchProductList, name: '个人流水记录' },
+            { path: '/unmatchProductList', component: unmatchProductList, name: '店铺流水记录' },
+            { path: '/unmatchProductList', component: unmatchProductList, name: '转赠记录' },
+            { path: '/unmatchProductList', component: unmatchProductList, name: '金豆记录' },
+            { path: '/unmatchProductList', component: unmatchProductList, name: '扫码付流水记录' },
+            { path: '/unmatchProductList', component: unmatchProductList, name: '公益筹款记录' },
+            { path: '/unmatchProductList', component: unmatchProductList, name: '商户手续费记录' }
         ]
     },
     {
@@ -193,7 +205,63 @@ let routes = [
     {
         path: '/',
         component: Home,
-        name: '系统设置',
+        name: '用户管理',
+        iconCls: 'el-icon-message',
+        children: [
+            { path: '/department', component: department, name: '员工管理' },
+            { path: '/menu', component: menu, name: '菜单管理' },
+            { path: '/role', component: role, name: '组织管理' },
+            // { path: '/account', component: account, name: '账号管理' },
+            { path: '/editPassword', component: editPassword, name: '修改密码' },
+            { path: '/operationLog', component: operationLog, name: '操作日志' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '扫码付管理',
+        iconCls: 'el-icon-message',
+        children: [
+            { path: '/department', component: department, name: '员工管理' },
+            { path: '/menu', component: menu, name: '菜单管理' },
+            { path: '/role', component: role, name: '组织管理' },
+            // { path: '/account', component: account, name: '账号管理' },
+            { path: '/editPassword', component: editPassword, name: '修改密码' },
+            { path: '/operationLog', component: operationLog, name: '操作日志' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '慈善公益管理',
+        iconCls: 'el-icon-message',
+        children: [
+            { path: '/department', component: department, name: '员工管理' },
+            { path: '/menu', component: menu, name: '菜单管理' },
+            { path: '/role', component: role, name: '组织管理' },
+            // { path: '/account', component: account, name: '账号管理' },
+            { path: '/editPassword', component: editPassword, name: '修改密码' },
+            { path: '/operationLog', component: operationLog, name: '操作日志' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '充值管理',
+        iconCls: 'el-icon-message',
+        children: [
+            { path: '/department', component: department, name: '员工管理' },
+            { path: '/menu', component: menu, name: '菜单管理' },
+            { path: '/role', component: role, name: '组织管理' },
+            // { path: '/account', component: account, name: '账号管理' },
+            { path: '/editPassword', component: editPassword, name: '修改密码' },
+            { path: '/operationLog', component: operationLog, name: '操作日志' }
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '网络电话管理',
         iconCls: 'el-icon-message',
         children: [
             { path: '/department', component: department, name: '员工管理' },
